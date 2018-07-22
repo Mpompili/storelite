871c99096d5318922d65cb0ccac9039b12d01c4d
Michael Pompili <michael@churroboros.com>
2018-07-21 18:21:37 -0700

    fixed listener bug

--- theStore.js
+++ theStore.js
@@ -1,8 +1,8 @@
-function reducer(state = {}, action) {
+function reducer(state = [], action) {
     //if we invoke this method with the ADD_TODO's action..
     if (action.type === 'ADD_TODO') {
         //return new state object to replace old. 
-        return Object.assign({}, state, [action.todo]);
+        return state.concat([action.todo]); 
     }
     //otherwise, 
     return state;
@@ -64,11 +64,9 @@ function createStore() {
     };
 
     const dispatch = (action) => { 
-        listeners.forEach(listener => listener());
-        
         // uses reducer to create to state to update store. 
-        
         state = reducer(state, action);  
+        listeners.forEach(listener => listener());
 
     };
 
